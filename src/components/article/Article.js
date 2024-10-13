108ba7c4190f5e2180f8f26630f1e72590eb2658
Gabriel D'Amboise <Gabriel.DAmboise@cssmi.qc.ca>
2024-10-13 12:42:50 -0400

    correct mainpage css for reddit articles

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,22 +1,23 @@
 import React from "react";
-import Lama from "../../ressources/images/1002997-Lama.jpg"
 
 export default function Article ({article}) {
     console.log(article)
     return (
         <div className="mainPageArticle borderedElement">
+            <div className="mainPageArticleButtonZone">
+                <button className="opinionButton">Upvote</button>
+                <p>{article.score}</p>
+                <button className="opinionButton">Downvote</button>
+            </div>
             <div className="mainPageArticleTextZone">
                 <h2>{article.title}</h2>
-                <div className="mainPageArticleButtonZone">
-                    <button className="opinionButton">Upvote</button>
-                    <p>{article.score}</p>
-                    <button className="opinionButton">Downvote</button>
-                </div>
+                {
+                    article.previewImage ? <img src={article.previewImage} alt="Article image if any"/> : ""
+                }
                 {
                     article.content ? <p className="scrollZones" style={{maxHeight: "300px"}}>{article.content}</p> : ""
                 }
             </div>
-            <img src={article.previewImage} alt="it's a lama" style={{margin: "auto", width: "40%", height: "auto"}}/>
         </div>
     );
 }
