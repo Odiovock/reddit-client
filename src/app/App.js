107bceeaa01f6de3ce51b1a5c8fc6944f48e3c06
Odiovock <64711277+Odiovock@users.noreply.github.com>
2024-11-01 14:09:37 -0400

    page title

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,14 @@ import { Route, BrowserRouter, Routes} from "react-router-dom";
 import AppLayout from './AppLayout';
 import MainPageArticle from '../features/MainPageArticles/MainPageArticles';
 import ArticlePage from '../features/ArticlePageArticle/ArticlePage';
+import { useEffect } from 'react';
 
 
 function App() {
+  useEffect(() => {
+    document.title = "Reddit web app";
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
